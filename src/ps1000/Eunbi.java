e83f2f72d4f573dc800308af2675a4952eca457a
Eunbi13 <eunbipro@gmail.com>
2022-05-12 13:06:00 +0900

    .

--- src/ps1000/Eunbi.java
+++ src/ps1000/Eunbi.java
@@ -1,9 +1,19 @@
 package ps1000;
 
+import java.util.Scanner;
+
 public class Eunbi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	  Scanner sc = new Scanner(System.in);
+	  String nums = sc.nextLine();
+	  String [] numArray = nums.split(" ");
+	  int a = Integer.parseInt(numArray[0]);
+	  int b = Integer.parseInt(numArray[1]);
+	  if(a>0 && b<10) {
+	    System.out.println(a+b);
+	  }
 	}
 
 }

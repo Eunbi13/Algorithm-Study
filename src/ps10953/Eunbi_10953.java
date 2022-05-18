2740aa3201f579cddbe5a8e8201bcca9e6bd8988
Eunbi13 <eunbipro@gmail.com>
2022-05-18 20:31:12 +0900

    .

---
+++ src/ps10953/Eunbi_10953.java
@@ -0,0 +1,25 @@
+package ps10953;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Eunbi_10953 {
+
+  public static void main(String[] args) throws Exception {
+    // TODO Auto-generated method stub
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int a = Integer.parseInt(br.readLine());
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st;
+    for(int i=0; i<a; i++) {
+      
+      st = new StringTokenizer(br.readLine());
+      
+    }
+    
+  }
+
+}

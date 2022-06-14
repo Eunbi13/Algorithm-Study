6780821ace639c48b3d6129599a47a4e271ac60a
Eunbi13 <eunbipro@gmail.com>
2022-06-14 13:16:12 +0900

    .

--- src/ps1463/Eunbi_1463.java
+++ src/ps1463/Eunbi_1463.java
@@ -1,42 +1,36 @@
 package ps1463;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Eunbi_1463 {
   
   static Integer [] memo;
 
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringBuffer sb = new StringBuffer();
     int n = Integer.parseInt(br.readLine());
+    long a = System.nanoTime();
     
-//    memo = new Integer[n];
     
-    
-    int i =0;
-   // System.out.println("@@@@@@@@@@@@");
-    //while(n!=1) {
-      n=dp(n);
-      System.out.println(n);
+    memo = new Integer[n+1];
+    memo[0]=memo[1]=0;
+    System.out.println(dp(n));
 
-      i++;
-   // }
-    System.out.println("@@@@@@@@@@@@");
-    System.out.println(i);
-
+    long b =  System.nanoTime();
+    System.out.println((b-a)+"||");
   }
   
-  public static int dp(int n) {
+  static int dp(int n) {
    if(memo[n] ==null) {
     if(n%6 == 0) {
-     n/=3; 
+     memo[n] =Math.min(dp(n-1) , Math.min( dp(n/3), dp(n/2))) +1;
     }
     else if(n % 3==0) {
-      n/=3;
+      memo[n] =  Math.min(dp(n-1), dp(n/3)) +1;
     }else if(n % 2==0) {
-      n/=2;
+      memo[n] =Math.min(dp(n-1)  ,dp(n/2))+1;
     }else {
       memo[n] = dp(n-1)+1;
     }

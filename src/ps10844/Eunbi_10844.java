bfe1e50968852b0213cde4aca499eefe33894568
Eunbi13 <eunbipro@gmail.com>
2022-06-21 00:29:43 +0900

    .,

--- src/ps10844/Eunbi_10844.java
+++ src/ps10844/Eunbi_10844.java
@@ -4,19 +4,74 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Eunbi_10844 {
-  
-//  static Integer [] m;
-
   public static void main(String[] args) throws Exception{
+    
+    one();
+    
+//    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//    
+//    int n = Integer.parseInt(br.readLine());
+//    int result = 0;
+//    long mod = 1000000000;
+//    long [][] m = new long [n+1][10];
+//    
+//    for(int i = 1; i < 10; i++) {
+//      m[1][i] = 1; 
+//    }
+//    
+//    for(int i = 2; i<= n; i++) {
+//      for(int j = 0; j<10; j++) {
+//        if(j==0) {
+//          m[i][j] = m[i-1][j+1]%mod;
+//        }else if(j==9) {
+//          m[i][j] = m[i-1][j-1]%mod;
+//        } else {
+//          m[i][j] = (m[i-1][j-1]+m[i-1][j+1])%mod;
+//        }
+//
+//    }
+//    }    
+//    for(int i = 0; i<10; i++) {
+//      result += (m[n][i] );
+//    }
+//    
+//    System.out.println( result%mod );
+//    br.close();
+  }
+  
+  
+  //런타임 에러 뜸
+  public static void one() throws Exception{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
-    Integer [][] m = new Integer [n+1][10];
-    for(int i = 0; i<10; i++) {
-      m[0][i] = 1;
+    int result = 0;
+    long a = 1000000000;
+    long [][] m = new long [n+1][10];
+    
+    for(int i = 1; i< 10; i++) {
+      m[1][i] = 1;
     }
     
-    System.out.println(m[n]);
+    for(int i = 2; i<= n; i++) {
+      for(int j = 0; j<10; j++) {
+        long x = 0, y = 0;
+        if(j-1 >=0) {
+          x = m[i-1][j-1]% a ;
+        }
+        if(j+1<10) {
+          y = m[i-1][j+1]% a ;
+        }
+        m[i][j] = (x+y)% a;
+      }
+    }
+    
+    for(int i = 1; i<10; i++) {
+      result += (m[n][i] % a);
+    }
+
+    System.out.println(result);
     br.close();
   }
+ 
 
 }
--- src/ps11057/Eunbi_11057.java
+++ src/ps11057/Eunbi_11057.java
@@ -8,6 +8,36 @@ public class Eunbi_11057 {
   public static void main(String[] args) throws Exception{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
+    long [][] m = new long [n+1][10];
+    long result = 0;
+    long mod = 10007;
+    for(int i = 0; i<10;i++) {
+      m[1][i]=1;
+    }
+    for(int i = 2; i<=n;i++) {
+      for(int j = 0; j<10; j++) {
+//======== 1번       
+        for(int k = 0; k<10; k++) {
+          if(k>=j) {
+            m[i][j] += m[i-1][k] ;            
+          }
+        }
+        m[i][j] %=mod;
+//======== 2번        
+//        long num = 0;
+//        for(int k = 0; k<10; k++) {
+//          if(k>=j) {
+//            num += m[i-1][k] ;            
+//          }
+//        }
+//        m[i][j] = num %mod;
+      }
+    }
+    for(int i = 0; i<10; i++) {
+      result += m[n][i];
+    }
+    System.out.println(result % mod);
+    br.close();
   }
 
 }

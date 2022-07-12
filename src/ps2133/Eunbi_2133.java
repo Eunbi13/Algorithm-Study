77ec9259f2abcc870cd76f27e32a222566e8667f
Eunbi13 <eunbipro@gmail.com>
2022-07-12 13:28:22 +0900

    .

--- src/ps2133/Eunbi_2133.java
+++ src/ps2133/Eunbi_2133.java
@@ -9,44 +9,34 @@ public class Eunbi_2133 {
 
   public static void main(String[] args) throws Exception{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int N = Integer.parseInt(br.readLine());
-    dp = new Integer [N+1];
-    
-    dp[0] = 0;
-    dp[2] = 3;
-    if(N%2==0) {
-      
-    for(int i=4; i<=N; i+=2) {
-      dp[i] = dp[i-2]*3 + dp[i-4]*2 +2; 
-    }
-    }
-    
-    System.out.println(dp[N]);
-//    
-//    dp = new Integer [N+1];
-//    dp[0] = 1;
-//    dp[2] = 3;
-//    
-//    for(int i=4; i<=N; i+=2) {
-//      dp[i] = dp[i-2] * 3 ;
-//      for(int j = 0 ; j<i-2; j +=2) {
-//        dp[i] = dp[i] + dp[j]*2 ;
-//      }
-//    }
-//    
-//    System.out.println(dp[N]);
-    
-//    int n = Integer.parseInt(br.readLine());
-//    dp =new Integer[n+1];
-//    dp[0]=1;
-//    
-//    for(int i=2;i<n+1;i=i+2) {
-//        dp[i]=dp[i-2]*3;
-//        for(int j=0;j<i-2;j=j+2) {
-//            dp[i]=dp[i]+dp[j]*2;
-//        }
-//    }
-//    System.out.println(dp[n]);
+  //   int N = Integer.parseInt(br.readLine());
+
+
+  //  dp = new Integer [N+1];
+  //  dp[0] = 1;
+  //  dp[2] = 3;
+
+  //  int tmp =0;
+  //  for(int i=4; i<=N; i+=2) {
+  //    dp[i] = dp[i-2] * 3 + 2;
+  //    for(int j = 2 ; j<i-2; j +=2) {
+  //     dp[i] += dp[j]*2 ;
+  //     }
+  //  }
+   
+  //  System.out.println(dp[N]);
+
+   int n = Integer.parseInt(br.readLine());
+   int [] dp = new int[n+4];
+   dp[0] = 1;
+   dp[2] = 3;
+   dp[4] = 11;
+   int tmp =0;
+   for (int i=6; i<=n; i+=2) {
+       dp[i] = dp[i-2] * 3 + 2 + (tmp+=dp[i-4]*2);
+   }
+
+   System.out.println(dp[n]);
   }
   
  

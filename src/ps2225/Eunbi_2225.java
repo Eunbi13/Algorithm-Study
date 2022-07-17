651876eeb622f3fb808dcba36434b56ffb366f35
Eunbi13 <eunbipro@gmail.com>
2022-07-18 00:26:55 +0900

    .

---
+++ src/ps2225/Eunbi_2225.java
@@ -0,0 +1,36 @@
+package ps2225;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Eunbi_2225 {
+  
+  static Long [][] P;
+
+  public static void main(String[] args) throws Exception{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int K = Integer.parseInt(br.readLine());
+    
+    P = new Long [N+1][K];
+    
+    for(int i = N; i>=0; i--) {
+      int x = N-i;
+      for(int j = 0; j<=x; j++) {
+
+      }
+      
+      
+      
+      
+    }
+    
+    System.out.println();
+    
+    
+  }
+  
+  
+  
+
+}

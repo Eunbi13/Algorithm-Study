514dbca44201a0c5e98af5e405b9ee0818e7137c
GGiYeol <87460258+GGiYeol@users.noreply.github.com>
2022-08-23 08:52:29 +0900

    gi

---
+++ src/ps2743/Giyeol2743.java
@@ -0,0 +1,13 @@
+package week14.ps2743;
+
+import java.util.Scanner;
+
+public class Giyeol2743 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String word = scan.nextLine();
+		
+		System.out.println(word.length());
+	}
+}

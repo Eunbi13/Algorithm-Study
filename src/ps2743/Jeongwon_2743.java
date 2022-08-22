17fc2ca25d38e330462c5e6c3ddb987829eecde2
KWAK JEONGWON <5000batt@gmail.com>
2022-08-23 01:26:06 +0900

    Jeongwon

---
+++ src/ps2743/Jeongwon_2743.java
@@ -0,0 +1,16 @@
+package ps2743;
+
+import java.util.Scanner;
+
+public class Jeongwon_2743 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		String s = sc.next();
+
+		System.out.println(s.length());
+	}
+
+}

03e6451a9407c1dde5efecc9cdca1c11a1eed4f5
곽정원 <jw208274@gmail.com>
2022-09-19 20:07:41 +0900

    jeongwon

---
+++ src/ps11576/Jeongwon_11576.java
@@ -0,0 +1,14 @@
+package ps11576;
+
+import java.util.Scanner;
+
+public class Jeongwon_11576 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        
+    }
+}
---
+++ src/ps1929/Jeongwon_1929.java
@@ -0,0 +1,16 @@
+package ps1929;
+
+import java.util.Scanner;
+
+public class Jeongwon_1929 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for (int i = n; i <= m; i++) {
+            
+        }
+    }
+}

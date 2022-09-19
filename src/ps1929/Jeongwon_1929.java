59e320c477d4752f1fadb7196e925b95ad0ef1c6
KWAK JEONGWON <5000batt@gmail.com>
2022-09-20 02:28:11 +0900

    jeongwon

--- src/ps1929/Jeongwon_1929.java
+++ src/ps1929/Jeongwon_1929.java
@@ -3,14 +3,25 @@ package ps1929;
 import java.util.Scanner;
 
 public class Jeongwon_1929 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+		int m = sc.nextInt();
+		int n = sc.nextInt();
 
-        for (int i = n; i <= m; i++) {
-            
-        }
-    }
+		boolean[] b = new boolean[n + 1];
+		b[0] = true;
+		b[1] = true;
+
+		for (int i = 2; i <= n; i++) {
+			if (b[i] == false) {
+				if (i >= m) {
+					System.out.println(i);
+				}
+				for (int j = i + i; j <= n; j += i) {
+					b[j] = true;
+				}
+			}
+		}
+	}
 }
